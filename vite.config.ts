b69f727dfcdb43ef40e0fd39e9f01398d5d136b6
Vokondo <167377625+Vokondo@users.noreply.github.com>
2025-05-15 09:02:32 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/seiku-website/',
+  base: '/seikuweb/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
