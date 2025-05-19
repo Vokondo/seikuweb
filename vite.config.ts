e273c20bed597c87913030adf363d0497c44a6e6
Vokondo <167377625+Vokondo@users.noreply.github.com>
2025-05-19 16:10:29 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/seikuweb/',
   build: {
     outDir: 'dist',
     sourcemap: true,
